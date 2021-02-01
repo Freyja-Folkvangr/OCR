7a89016d3a57067060ecf8be3343087781d53550
Giuliano Tognarelli <giuliano150@gmail.com>
2021-02-01 18:35:28 -0300

    added method to check if the url: str contains a valid url, evaluate contents of url

--- mysite/schema.py
+++ mysite/schema.py
@@ -1,24 +1,26 @@
 import graphene, re
 from graphene import String
 
+from ocr.eval import evaluate
+
+
+def is_url(url):
+    # i hate regex
+    # https://ihateregex.io/expr/url/
+    x = re.search(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)", url)
+    if x:
+        return True
+    else:
+        return False
+
+
 class Query(graphene.ObjectType):
     ocr = graphene.String(url=String(default_value=None))
 
-    def is_url(self, url):
-        # i hate regex
-        # https://ihateregex.io/expr/url/
-        x = re.search(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)", url)
-        if x:
-            return True
-        else:
-            return False
-
-
     def resolve_ocr(self, root, url):
         print('the url is %s' % url)
-        if url and self.is_url(url):
-            # do something
-            return url
+        if url and is_url(url):
+            return evaluate(url)
         else:
             raise Exception('Invalid URL or no URL provided')
 
