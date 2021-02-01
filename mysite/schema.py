13abecaca0bfd33ecbb2a728a897c4c48854a150
Giuliano Tognarelli <giuliano150@gmail.com>
2021-02-01 17:35:05 -0300

    skeleton for ocr graphql query

--- mysite/schema.py
+++ mysite/schema.py
@@ -1,6 +1,17 @@
 import graphene
+from graphene import String
+
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hi!")
+    ocr = graphene.String(url=String(default_value=None))
+
+    def resolve_ocr(self, root, url):
+        print('the url is %s' % url)
+        if url:
+            # do something
+            return url
+        else:
+            return 'no url'
+
 
 schema = graphene.Schema(query=Query)
