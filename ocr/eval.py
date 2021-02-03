8d7b162aa6a882f2b3bd64104e4296a7d351c487
Giuliano Tognarelli <giuliano150@gmail.com>
2021-02-03 17:07:57 -0300

    added todo for image resizing

--- ocr/eval.py
+++ ocr/eval.py
@@ -33,6 +33,9 @@ def load_image(img_path):
 def load_image_from_url(url):
     # download the image, convert it to a NumPy array, and then read
     # it into OpenCV format
+
+    # TODO resize the image according to https://blog.roboflow.com/you-might-be-resizing-your-images-incorrectly/
+
     resp = urllib.request.urlopen(url)
     image = np.asarray(bytearray(resp.read()), dtype="uint8")
     image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
